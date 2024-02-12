b02f46490ed49417a32c11a02ecf0bb7dce7c8a9
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-02-13 02:31:07 +0530

    Learned uploading the files

---
+++ src/middlewares/multer.midleware.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req,file,cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req,file,cb) {
+        
+        // Yahan pe modification kiya ja sakta hai file ke name me
+        // file ko console log kara ke dekh lena bahut kuch seekhne ko milega
+        cb(null, file.originalname)
+    }
+})
+
+export const upload = multer({
+    storage
+})
