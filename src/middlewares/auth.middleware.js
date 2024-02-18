104690010bf82dc98115e34affed9f4c48b71d8d
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-02-18 15:31:49 +0530

    Some more changes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,6 +2,8 @@ import { ApiError } from "../utils/ApiErrors";
 import { asyncHandler } from "../utils/asyncHandler";
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.model";
+
+
 export const verifyJWT = asyncHandler( async (req, _,next) => {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
 
@@ -11,6 +13,7 @@ export const verifyJWT = asyncHandler( async (req, _,next) => {
         }
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        console.log("Here I am seeing what is in decodedToken ",decodedToken);
     
         const user = await User.findById(decodedToken._id)
         .select("-password -refreshToken")
